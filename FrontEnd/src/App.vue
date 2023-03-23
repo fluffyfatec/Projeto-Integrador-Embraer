d84b134da0c12ce8f141a222159667c1992dafc0
Felipe Sobral <felipesobral_@hotmail.com>
2023-03-23 05:16:46 -0300

    Criação de componentes

--- FrontEnd/src/App.vue
+++ FrontEnd/src/App.vue
@@ -28,14 +28,7 @@
         </div>  
       </nav>
 
-      <div class="container-pesquisa">
-        <div class="container-pesquisa">
-          <div class="pesquisar">
-              <input v-if="$route.meta.showPesquisar" type="text" class="formulario" :placeholder="$route.meta.placeHolderValue">
-              <h3 v-if="$route.meta.showH3">{{ $route.meta.tituloValue }}</h3>
-          </div>
-        </div>
-      </div>
+      <ContainerPesquisa></ContainerPesquisa>
 
     </div>  
 
@@ -58,6 +51,7 @@
 
 
 <script lang="ts">
+import ContainerPesquisa from './components/ContainerPesquisa.vue'
 
 export default {
   data() {
@@ -83,6 +77,9 @@ export default {
   beforeDestroy() {
     window.removeEventListener('resize', this.checkIfMobile);
   },
+  components: {
+    ContainerPesquisa,
+  }
 };
 </script>
 
@@ -192,6 +189,7 @@ footer {
   opacity: 0.7;
   transition: 0.1s;
 }
+
 /* Classe wrapper */
 .wrapper {
   background-image: url(@/assets/aeroporto.jpg);
@@ -200,55 +198,7 @@ footer {
   background-position: center center;
   height: 377px;
 }
-/* Container Pesquisa */
-.container-pesquisa {
-  display: -webkit-box;
-  display: -ms-flexbox;
-  display: flex;
-  -webkit-box-align: center;
-      -ms-flex-align: center;
-          align-items: center;
-  -webkit-box-pack: center;
-      -ms-flex-pack: center;
-          justify-content: center;
-  margin-top: 8%;
-}
-/* Classes referentes ao formulário de pesquisa */
-.pesquisar {
-    display: -webkit-box;
-    display: -ms-flexbox;
-    display: flex;
-    -webkit-box-align: stretch;
-        -ms-flex-align: stretch;
-            align-items: stretch;
-    -webkit-box-pack: center;
-        -ms-flex-pack: center;
-            justify-content: center;
-    -webkit-box-orient: vertical;
-    -webkit-box-direction: normal;
-        -ms-flex-direction: column;
-            flex-direction: column;
-    position: relative;
-}
-.pesquisar h3 {
-    -ms-flex-item-align: center;
-        align-self: center;
-    margin-right: 1rem;
-    font-size: 2rem;
-    color: var(--azul-profundo);
-    font-weight: 500;
-    margin-bottom: 1.5rem;
-}
-.pesquisar .formulario {
-    opacity: 0.6;
-    padding: 1rem 2rem;
-    font-size: 18pt;
-    border: none;
-    border-radius: 5px;
-    outline: none;
-    color: var(--light-dark-color);
-    width: 500px;
-}
+
 /* --------------- Media Queries -------------------- */
 /* Estilos para tablet */
 @media only screen and (min-width: 768px) and (max-width: 1023px) {
@@ -256,63 +206,16 @@ footer {
 .wrapper {
   background-image: url(@/assets/aeroporto.jpg);
   height: 200px;
+} 
 }
-/* Container Pesquisa */
-.container-pesquisa {
-  margin-top: 6%;
-}
-/* Classes referentes ao formulário de pesquisa */
-.pesquisar h3 {
-    margin-right: 1rem;
-    font-size: 2rem;
-    color: var(--azul-profundo);
-    font-weight: 500;
-    margin-bottom: 1.5rem;
-}
-.pesquisar .formulario {
-    opacity: 0.7;
-    padding: 1rem 1rem;
-    font-size: 14pt;
-    border: none;
-    border-radius: 5px;
-    outline: none;
-    color: var(--light-dark-color);
-    width: 300px;
-    height: auto;
-}
-  
-}
+
 /* Estilos para mobile */
 @media only screen and (max-width: 767px) {
+
   /* Classe wrapper */
 .wrapper {
   background-image: url(@/assets/aeroporto-mobile.jpg);
   height: 188px;
-}
-/* Container Pesquisa */
-.container-pesquisa {
-  margin-top: 9%;
-}
-/* Classes referentes ao formulário de pesquisa */
-.pesquisar h3 {
-    -ms-flex-item-align: center;
-        align-self: center;
-    margin-right: 1rem;
-    font-size: 2rem;
-    color: var(--azul-profundo);
-    font-weight: 500;
-    margin-bottom: 1.5rem;
-}
-.pesquisar .formulario {
-    opacity: 0.7;
-    padding: 1rem 0.8rem;
-    font-size: 12pt;
-    border: none;
-    border-radius: 5px;
-    outline: none;
-    color: var(--light-dark-color);
-    width: 180px;
-    height: 10px;
 }
 }
 </style>
---
+++ FrontEnd/src/components/ContainerPesquisa.vue
@@ -0,0 +1,126 @@
+<template>
+    <div class="container-pesquisa">
+        <div class="container-pesquisa">
+          <div class="pesquisar">
+              <input v-if="$route.meta.showPesquisar" type="text" class="formulario" :placeholder="$route.meta.placeHolderValue">
+              <h3 v-if="$route.meta.showH3">{{ $route.meta.tituloValue }}</h3>
+          </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+
+/* Container Pesquisa */
+.container-pesquisa {
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-align: center;
+      -ms-flex-align: center;
+          align-items: center;
+  -webkit-box-pack: center;
+      -ms-flex-pack: center;
+          justify-content: center;
+  margin-top: 8%;
+}
+
+/* Classes referentes ao formulário de pesquisa */
+.pesquisar {
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-align: stretch;
+        -ms-flex-align: stretch;
+            align-items: stretch;
+    -webkit-box-pack: center;
+        -ms-flex-pack: center;
+            justify-content: center;
+    -webkit-box-orient: vertical;
+    -webkit-box-direction: normal;
+        -ms-flex-direction: column;
+            flex-direction: column;
+    position: relative;
+}
+.pesquisar h3 {
+    -ms-flex-item-align: center;
+        align-self: center;
+    margin-right: 1rem;
+    font-size: 2rem;
+    color: var(--azul-profundo);
+    font-weight: 500;
+    margin-bottom: 1.5rem;
+}
+.pesquisar .formulario {
+    opacity: 0.6;
+    padding: 1rem 2rem;
+    font-size: 18pt;
+    border: none;
+    border-radius: 5px;
+    outline: none;
+    color: var(--light-dark-color);
+    width: 500px;
+}
+
+/* --------------- Media Queries -------------------- */
+
+/* Estilos para tablet */
+@media only screen and (min-width: 768px) and (max-width: 1023px) {
+  
+/* Container Pesquisa */
+.container-pesquisa {
+  margin-top: 6%;
+}
+/* Classes referentes ao formulário de pesquisa */
+.pesquisar h3 {
+    margin-right: 1rem;
+    font-size: 2rem;
+    color: var(--azul-profundo);
+    font-weight: 500;
+    margin-bottom: 1.5rem;
+}
+.pesquisar .formulario {
+    opacity: 0.7;
+    padding: 1rem 1rem;
+    font-size: 14pt;
+    border: none;
+    border-radius: 5px;
+    outline: none;
+    color: var(--light-dark-color);
+    width: 300px;
+    height: auto;
+}
+  
+}
+/* Estilos para mobile */
+@media only screen and (max-width: 767px) {
+
+/* Container Pesquisa */
+.container-pesquisa {
+  margin-top: 9%;
+}
+/* Classes referentes ao formulário de pesquisa */
+.pesquisar h3 {
+    -ms-flex-item-align: center;
+        align-self: center;
+    margin-right: 1rem;
+    font-size: 2rem;
+    color: var(--azul-profundo);
+    font-weight: 500;
+    margin-bottom: 1.5rem;
+}
+.pesquisar .formulario {
+    opacity: 0.7;
+    padding: 1rem 0.8rem;
+    font-size: 12pt;
+    border: none;
+    border-radius: 5px;
+    outline: none;
+    color: var(--light-dark-color);
+    width: 180px;
+    height: 10px;
+}
+}
+
+</style>
