46a930a4362a70e6195754c7a4fe7fcf5c809c76
Júlia Rafaela <julia.silva71@fatec.sp.gov.br>
2023-03-23 16:48:31 -0300

    change in layout and responsiveness

--- FrontEnd/src/components/ContainerPesquisa.vue
+++ FrontEnd/src/components/ContainerPesquisa.vue
@@ -4,6 +4,7 @@
           <div class="pesquisar">
               <input v-if="$route.meta.showPesquisar" type="text" class="formulario" :placeholder="$route.meta.placeHolderValue">
               <h3 v-if="$route.meta.showH3">{{ $route.meta.tituloValue }}</h3>
+              <button v-if="$route.meta.showPesquisar" id="btnBusca"><i class="fa-solid fa-magnifying-glass fa" style="color: #ffffff;"></i></button>
           </div>
         </div>
     </div>
@@ -23,7 +24,7 @@
   -webkit-box-pack: center;
       -ms-flex-pack: center;
           justify-content: center;
-  margin-top: 8%;
+  margin-top: 5%;
 }
 
 /* Classes referentes ao formulário de pesquisa */
@@ -41,14 +42,15 @@
     -webkit-box-direction: normal;
         -ms-flex-direction: column;
             flex-direction: column;
-    position: relative;
+    position: absolute;
+    flex-direction: row;
 }
 .pesquisar h3 {
     -ms-flex-item-align: center;
         align-self: center;
     margin-right: 1rem;
     font-size: 2rem;
-    color: var(--azul-profundo);
+    color: var(--azul-principal);
     font-weight: 500;
     margin-bottom: 1.5rem;
 }
@@ -57,10 +59,38 @@
     padding: 1rem 2rem;
     font-size: 18pt;
     border: none;
-    border-radius: 5px;
+    border-radius: 5px 0 0 5px;
     outline: none;
     color: var(--light-dark-color);
-    width: 500px;
+    width: 250px;
+}
+
+#btnBusca{
+  border:none;
+  float:left;
+  height:40px;
+  border-radius: 0 5px 5px 0;
+  width:100px;
+  background: var(--azul-principal);
+  height: auto;
+  transition-duration: 0.4s;
+  cursor: pointer;
+  transition: 0.1s all;
+}
+
+#btnBusca:hover {
+  background-color: var(--azul-claro);
+  color: white;
+}
+
+#btnBusca:active {
+  transform: scale(1);
+  box-shadow: 1px 1px 1px 1px var(--azul-principal);
+}
+
+.fa{
+    width: 100%;
+    font-size: 30px;
 }
 
 /* --------------- Media Queries -------------------- */
@@ -70,28 +100,59 @@
   
 /* Container Pesquisa */
 .container-pesquisa {
-  margin-top: 6%;
+    margin-top: 4%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 /* Classes referentes ao formulário de pesquisa */
 .pesquisar h3 {
     margin-right: 1rem;
     font-size: 2rem;
-    color: var(--azul-profundo);
+    color: var(--azul-principal);
     font-weight: 500;
     margin-bottom: 1.5rem;
 }
 .pesquisar .formulario {
-    opacity: 0.7;
-    padding: 1rem 1rem;
+    opacity: 0.6;
+    padding: 1rem 2rem;
     font-size: 14pt;
     border: none;
-    border-radius: 5px;
+    border-radius: 5px 0 0 5px;
     outline: none;
     color: var(--light-dark-color);
-    width: 300px;
-    height: auto;
+    width: 150px;
+    height: 5px;
 }
-  
+
+#btnBusca{
+  border:none;
+  float:left;
+  height:40px;
+  border-radius: 0 5px 5px 0;
+  width:100px;
+  background: var(--azul-principal);
+  height: auto;
+  transition-duration: 0.4s;
+  cursor: pointer;
+  transition: 0.1s all;
+}
+
+#btnBusca:hover {
+  background-color: var(--azul-claro);
+  color: white;
+}
+
+#btnBusca:active {
+  transform: scale(1);
+  box-shadow: 1px 1px 1px 1px var(--azul-principal);
+}
+
+.fa{
+    width: 100%;
+    font-size: 20px;
+}
+
 }
 /* Estilos para mobile */
 @media only screen and (max-width: 767px) {
@@ -105,22 +166,51 @@
     -ms-flex-item-align: center;
         align-self: center;
     margin-right: 1rem;
-    font-size: 2rem;
-    color: var(--azul-profundo);
+    font-size: 1.5rem;
+    color: var(--azul-principal);
     font-weight: 500;
     margin-bottom: 1.5rem;
 }
 .pesquisar .formulario {
-    opacity: 0.7;
-    padding: 1rem 0.8rem;
-    font-size: 12pt;
+    opacity: 0.6;
+    padding: 1rem 2rem;
+    font-size: 10pt;
     border: none;
-    border-radius: 5px;
+    border-radius: 5px 0 0 5px;
     outline: none;
     color: var(--light-dark-color);
-    width: 180px;
-    height: 10px;
+    width: 100px;
+    height: 5px;
 }
+
+#btnBusca{
+  border:none;
+  float:left;
+  height:40px;
+  border-radius: 0 5px 5px 0;
+  width:70px;
+  background: var(--azul-principal);
+  height: auto;
+  transition-duration: 0.4s;
+  cursor: pointer;
+  transition: 0.1s all;
+}
+
+#btnBusca:hover {
+  background-color: var(--azul-claro);
+  color: white;
+}
+
+#btnBusca:active {
+  transform: scale(1);
+  box-shadow: 1px 1px 1px 1px var(--azul-principal);
+}
+
+.fa{
+    width: 100%;
+    font-size: 15px;
+}
+
 }
 
 </style>
