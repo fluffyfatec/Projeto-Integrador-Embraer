af532784d5a7abc6f04e28f830d610330db356d3
Victor Fernandes <victor.santos136@fatec.sp.gov.br>
2023-03-26 20:08:19 -0300

    mapping new table

--- backend/src/main/java/com/embraer/backend/usersb/repositories/UserServiceBulletinRepository.java
+++ backend/src/main/java/com/embraer/backend/usersb/repositories/UserServiceBulletinRepository.java
@@ -1,9 +1,11 @@
-package com.embraer.backend.usersb.repository;
+package com.embraer.backend.usersb.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.embraer.backend.usersb.entity.UserServiceBulletin;
 
+@Repository
 public interface UserServiceBulletinRepository extends JpaRepository<UserServiceBulletin, Long>{
-
+	
 }
