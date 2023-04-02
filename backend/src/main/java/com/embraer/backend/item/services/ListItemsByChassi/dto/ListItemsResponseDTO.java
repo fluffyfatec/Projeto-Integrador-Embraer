36ae8d5af1eaa1d767c1f5471222564a673ca3a7
unknown <lfborgesrp1@gmail.com>
2023-04-01 22:02:27 -0300

    Adjust on method RegisterBulletin and ListBulletins

--- backend/src/main/java/com/embraer/backend/item/services/ListItemsByChassi/dto/ListItemsResponseDTO.java
+++ backend/src/main/java/com/embraer/backend/item/services/ListItemsByChassi/dto/ListItemsResponseDTO.java
@@ -4,7 +4,6 @@ import lombok.Data;
 
 @Data
 public class ListItemsResponseDTO {
-
 	
 	private String name_item;
 	
--- backend/src/main/java/com/embraer/backend/item/services/ListItemsByChassi/util/ContainsItems.java
+++ backend/src/main/java/com/embraer/backend/item/services/ListItemsByChassi/util/ContainsItems.java
@@ -17,6 +17,8 @@ public class ContainsItems {
 		
 		
 		
+		
+		
 		return listItemsResponseDTOs;
 		
 		
