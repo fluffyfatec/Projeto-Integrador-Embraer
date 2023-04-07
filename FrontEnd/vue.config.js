724e5ccaccb395dd092670dca4dd859e752d1dac
Felipe Sobral <felipesobral_@hotmail.com>
2023-04-07 15:18:05 -0300

    Add vue configs

--- FrontEnd/vue.config.js
+++ FrontEnd/vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     devServer: {
       proxy: {
-        '/register': {
+        '/login': {
           target: 'http://localhost:8080',
           changeOrigin: true
         }
