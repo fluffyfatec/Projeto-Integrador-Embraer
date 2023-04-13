7b74cd1796515f41456ae45a1c78d1d570809bbe
Felipe Sobral <felipesobral_@hotmail.com>
2023-04-13 00:04:42 -0300

    Create queries in repositories

--- backend/src/main/java/com/embraer/backend/item/repositories/ItemRepository.java
+++ backend/src/main/java/com/embraer/backend/item/repositories/ItemRepository.java
@@ -1,11 +1,26 @@
 package com.embraer.backend.item.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.embraer.backend.chassis.entity.Chassis;
 import com.embraer.backend.item.entity.Item;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface ItemRepository extends JpaRepository<Item, Long>{
+
+    @Query("SELECT i.itemId FROM Item i WHERE i.itemName = :itemName")
+    Long findItemIdByItemName(@Param("itemName") String itemName);
+
+    @Query("SELECT i.itemName FROM Item i WHERE i.itemId = :itemId")
+    String findItemNameByItemId(@Param("itemId") Long itemId);
+
+    @Modifying
+    @Query("DELETE FROM Item i WHERE i.itemId = :itemId")
+    @Transactional
+    void deleteByItemId(@Param("itemId") Long itemId);
 
 }
--- backend/src/main/java/com/embraer/backend/serviceBulletin/repositories/ServiceBulletinRepository.java
+++ backend/src/main/java/com/embraer/backend/serviceBulletin/repositories/ServiceBulletinRepository.java
@@ -6,11 +6,27 @@ import com.embraer.backend.chassis.entity.Chassis;
 import com.embraer.backend.serviceBulletin.entity.ServiceBulletin;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ServiceBulletinRepository extends JpaRepository<ServiceBulletin, Long>{
 		
 	ServiceBulletin findByServiceBulletinNameAndServiceBulletinPart(String bulletinName, String bulletinPart);
+
+	@Query("SELECT sb.serviceBulletinId FROM ServiceBulletin sb WHERE sb.serviceBulletinName = :sbName AND sb.serviceBulletinPart = :sbPartService")
+	Long findServiceBulletinIdBySbNameAndSbPartService(@Param("sbName") String sbName, @Param("sbPartService") String sbPartService);
+
+	@Query("SELECT sb.serviceBulletinName FROM ServiceBulletin sb WHERE sb.serviceBulletinId = :sbId")
+	String findSbNameByServiceBulletinId(@Param("sbId") Long sbId);
+
+	@Query("SELECT sb.serviceBulletinPart FROM ServiceBulletin sb WHERE sb.serviceBulletinId = :sbId")
+	String findSbPartByServiceBulletinId(@Param("sbId") Long sbId);
+
+
+
+
 }
 
