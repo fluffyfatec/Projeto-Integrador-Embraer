60e5a324f1f91f845fc110ed90494443d3178f0d
Felipe Sobral <felipesobral_@hotmail.com>
2023-05-02 21:16:12 -0300

    Add verification of access levels

--- FrontEnd/src/components/ReturnChassis.vue
+++ FrontEnd/src/components/ReturnChassis.vue
@@ -13,6 +13,7 @@
 <script lang="ts">
 import axios from 'axios';
 import { eventBus } from '@/main';
+import globalData from '@/globals';
 
 export default {
 
@@ -20,6 +21,7 @@ export default {
         return {
             chassis: [],
             searchTerm: '',
+            g: globalData,
         }
     },
 
@@ -31,8 +33,22 @@ export default {
     methods: {
 
         async getChassis() {
+
+            if (this.g.userRole == 'ADMIN') {
             const response = await axios.get('http://localhost:8080/chassi/list');
             this.chassis = response.data.map((item: String) => ({ chassi_id: item.chassi_id }));
+            };
+
+            if (this.g.userRole == 'EDITOR') {
+                const response = await axios.get('http://localhost:8080/chassi/list/editor');
+                this.chassis = response.data.map((item: String) => ({ chassi_id: item.chassi_id }));
+            };
+
+            if (this.g.userRole == 'PILOT') {
+                const response = await axios.get('http://localhost:8080/chassi/list/' + this.g.userUserName);
+                this.chassis = response.data.map((item: String) => ({ chassi_id: item.chassi_id }));
+            };
+
         },
 
         divClickToItems(chassis: string) {
