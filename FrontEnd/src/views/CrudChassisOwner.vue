5e73b5a9668598928457a67ab1fad80710fefdf4
Felipe Sobral <felipesobral_@hotmail.com>
2023-05-03 22:15:50 -0300

    Add GlobalData

--- FrontEnd/src/views/CrudChassisOwner.vue
+++ FrontEnd/src/views/CrudChassisOwner.vue
@@ -8,8 +8,24 @@
 <script lang="ts">
 import RegisterChassisOwner from '@/components/RegisterChassisOwner.vue';
 import ReadDeleteChassisOwner from '@/components/ReadDeleteChassisOwner.vue';
+import globalData from '@/globals';
 
 export default {
+
+    data() {
+        return {
+            g: globalData,
+        }
+    },
+
+    mounted() {
+        if (this.g.userRole == "ADMIN") {
+            return
+        } else {
+            this.$router.push('/items');
+        }
+
+    },
 
     components: {
         RegisterChassisOwner,
